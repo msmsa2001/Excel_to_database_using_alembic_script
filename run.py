0bc529f9c62944b6d9603c6320901ce42f15525a
msmsa2001 <msmsa251001@gmail.com>
2023-08-14 20:38:58 +0530

    get data in relation table

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
 
@@ -11,6 +11,7 @@ class Topic(db.Model):
     __tablename__ = "topic_name"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    subtopics = db.relationship('SubTopic', back_populates='topic')  # Corrected the back_populates attribute name
 
 class SubTopic(db.Model):
     __tablename__ = "subtopic_name"
@@ -18,6 +19,19 @@ class SubTopic(db.Model):
     name = db.Column(db.String(100))
     topic_id = db.Column(
         db.Integer, 
-        db.ForeignKey('topic_name.id'),
-        )
+        db.ForeignKey('topic_name.id')
+    )
+    topic = db.relationship('Topic', back_populates='subtopics')  # Corrected the back_populates attribute name
 
+@app.route('/', methods=['GET'])
+def index():
+    data = {}
+
+    topics = Topic.query.all()
+    for topic in topics:
+        data[topic.name] = [{subtopic.name:subtopic.id} for subtopic in topic.subtopics]
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
